5fa3b46d2f05f5cc825ffeee25c1d6a770b6274a
danielpanjaitan <daniel@tanihub.com>
2018-11-15 16:28:07 +0700

    v0.8.0

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
     "github.com/blang/semver"
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
 )
-const version = "0.7.0"
+const version = "0.8.0"
 
 func selfUpdate(slug string) error {
 	selfupdate.EnableLog()
